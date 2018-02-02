b04800d44961c83e6c5d03fb3b016f6076c183a0
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2018-02-02 16:54:39 +0700

    refactor

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -14,12 +14,26 @@ class ViewController: UIViewController {
     
     let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
+    func updateDiceImage(){
+        // arc4random_uniform doesn't return Int, so we have to convert it to int first
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        // access the "image" property from diceImageView1
+        // UIImage is the data type
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // componentDidMount
+        updateDiceImage()
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,16 +42,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        // arc4random_uniform doesn't return Int, so we have to convert it to int first
-        randomDiceIndex2 = Int(arc4random_uniform(6))
+        // onClick
+        updateDiceImage()
         
-        // access the "image" property from diceImageView1
-        // UIImage is the data type
-        
-        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
-    
+
+
     
 }
